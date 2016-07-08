2ccb681b2b39817342daefd14e5cde95eaeb0ec5
gruteogisu <gruteogisu@gmail.com>
2016-07-08 17:28:00 +0900

    Fix pep8 error: according to pep8 guideline

--- typeYou/typeYou/urls.py
+++ typeYou/typeYou/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from typeYou.views import home 
+from typeYou.views import home
 
 
-urlpatterns = [                
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
-  
-    # typeYou Urls             
-    url(r'^$', home, name='home'),  
-    
-    # Included Apps Urls       
+
+    # typeYou Urls
+    url(r'^$', home, name='home'),
+
+    # Included Apps Urls
     url(r'^', include('users.urls', namespace='users')),
     url(r'^quizzes/', include('quizzes.urls', namespace='quizzes')),
 ]
