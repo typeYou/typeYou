77a6d0135af6470e72d4eea0451499a8904fa0ba
pn101 <philipnam101@gmail.com>
2016-07-09 10:35:21 +0900

    add permission required to TeacherMyPageView

--- typeYou/users/views/teachers/mypage.py
+++ typeYou/users/views/teachers/mypage.py
@@ -1,8 +1,10 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
-class TeacherMyPageView(TemplateView):
+class TeacherMyPageView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
     template_name = 'teachers/mypage.html'
+    permission_required = 'users.is_teacher'
 
     def get_context_data(self, **kwargs):
         context = super(TeacherMyPageView, self).get_context_data(**kwargs)
