99cff7ea6e4594fd78ef2c2098a26c6980b14eb1
pn101 <philipnam101@gmail.com>
2016-07-12 17:27:41 +0900

    init Follow model between BaseUser

--- typeYou/users/models/user.py
+++ typeYou/users/models/user.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 
 class BaseUser(AbstractUser):
+
+    follower_set = models.ManyToManyField(
+            'self',
+            symmetrical=False,
+            related_name='following_set',
+            through='Follow',
+            through_fields=('following', 'follower'),
+    )
+
     phonenumber = models.CharField(
             max_length=16,
     )
