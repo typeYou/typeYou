2528c01f27e0e4d9e552c62ec7cf85a8a58d5424
gruteogisu <gruteogisu@gmail.com>
2016-07-15 06:10:22 +0900

    Init Question edit feature
    add button to help user navigate web site
    make base_detail.html to be used at any page when it show questions

--- typeYou/quizzes/urls.py
+++ typeYou/quizzes/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
 
     url(r'^(?P<slug>\w+)/$', QuizView.as_view(), name="quiz_detail"),
     url(r'^(?P<slug>\w+)/edit/$', QuizEditView.as_view(), name="quiz_edit"),
-
-    url(r'^(?P<slug>\w+)/create/$', QuizQuestionCreateView.as_view(), name="question_create"),
-
     url(r'^(?P<slug>\w+)/publish/$', QuizPublishView.as_view(), name="publish"),
 
+    url(r'^(?P<slug>\w+)/question/create/$', QuizQuestionCreateView.as_view(), name="question_create"),
+    url(r'^(?P<slug1>\w+)/question/(?P<slug2>\w+)/edit/$', QuizQuestionEditView.as_view(), name="question_edit"),
+    url(r'^(?P<slug1>\w+)/question/(?P<slug2>\w+)/update/$', QuizQuestionUpdateView.as_view(), name="question_update"),
 ]
--- typeYou/quizzes/views/question/__init__.py
+++ typeYou/quizzes/views/question/__init__.py
@@ -1,1 +1,3 @@
 from .create import QuizQuestionCreateView
+from .edit import QuizQuestionEditView
+from .update import QuizQuestionUpdateView
