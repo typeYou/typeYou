58814969a9473737d42124e44f3d9d85ee274d24
pn101 <philipnam101@gmail.com>
2016-07-15 15:14:29 +0900

    hotfix FollowCreateView:
    updated FollowCreateView to only create one instance of a follower-following relationship

--- typeYou/users/views/follow/follow.py
+++ typeYou/users/views/follow/follow.py
@@ -2,6 +2,8 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.conf import settings
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 
 from users.models import BaseUser, Follow
 
@@ -16,14 +18,15 @@ class FollowCreateView(LoginRequiredMixin, CreateView):
         following = BaseUser.objects.get(
                 username=self.request.POST.get('username')
         )
-        # TODO: ensure that FollowCreateView object can only be created once
 
         form.instance.follower = follower
         form.instance.following = following
 
-        messages.add_message(
-                self.request,
-                messages.SUCCESS,
-                settings.FOLLOW_SUCCESS_MESSAGE,
-        )
-        return super(FollowCreateView, self).form_valid(form)
+        if Follow.objects.get_or_create(follower=follower, following=following)[1] is True:
+            messages.add_message(
+                    self.request,
+                    messages.SUCCESS,
+                    settings.FOLLOW_SUCCESS_MESSAGE,
+            )
+            return super(FollowCreateView, self).form_valid(form)
+        return redirect(reverse('users:teacherpage', kwargs={'slug': form.instance.following.username}))
