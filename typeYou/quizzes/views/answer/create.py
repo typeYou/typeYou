e9f0d2e4ac40ab160df1f3a5dfe7944b1fe5dd8a
gruteogisu <gruteogisu@gmail.com>
2016-07-20 21:41:59 +0900

    Add a connection between quiz and user when answer is created

--- typeYou/quizzes/views/answer/create.py
+++ typeYou/quizzes/views/answer/create.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.views.generic import View
 
-from quizzes.models import Question, Quiz
+from quizzes.models import Question, Quiz, Solve
 
 
 class AnswerCreateView(View):
@@ -18,8 +18,14 @@ class AnswerCreateView(View):
         if request.user == quiz.user:
             return redirect(reverse("home"))
 
+        # TODO: prevent Answer submit if it exist already
+        if quiz in request.user.solve_quiz_set.all():  # if answer is already submitted
+            pass
+
         for index, question in enumerate(quiz.question_set.public()):
             answer = request.POST.get('answer-{index}'.format(index=index+1))
             q = request.user.answer_set.create(ans=answer, quiz=question.quiz, question=question)
+
+        Solve.objects.create(user=request.user, quiz=quiz)
 
         return redirect("/login/")  # TODO: fix to direct correctly
