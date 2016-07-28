e82f3ea812da3b534194a8e8530e284cddc70b89
gruteogisu <gruteogisu@gmail.com>
2016-07-28 21:20:44 +0900

    Update home page: make the page look good

--- typeYou/typeYou/static/js/init.js
+++ typeYou/typeYou/static/js/init.js
@@ -1,9 +1,6 @@
 (function($){
   $(function(){
 
-//  $('.button-collapse').sideNav();
-//  $('.parallax').parallax();
-
 // Initialize collapse button
 $('.button-collapse').sideNav({
       edge: 'left',
@@ -11,6 +8,14 @@ $('.button-collapse').sideNav({
   );
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
+
+//  $('.parallax').parallax();
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
