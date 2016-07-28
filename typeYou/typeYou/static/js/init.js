58205d323bac1b19df6d51cc2cd1904c3dadddd8
gruteogisu <gruteogisu@gmail.com>
2016-07-28 15:42:35 +0900

    Update header.html: add SideNav

--- typeYou/typeYou/static/js/init.js
+++ typeYou/typeYou/static/js/init.js
@@ -4,5 +4,13 @@
 //  $('.button-collapse').sideNav();
 //  $('.parallax').parallax();
 
+// Initialize collapse button
+$('.button-collapse').sideNav({
+      edge: 'left',
+    }
+  );
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
